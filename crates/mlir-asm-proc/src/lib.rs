eede234e054a198ccb5fc2ca64d9e4836214a327
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-04-14 22:42:08 +0200

    Add module generation.

--- crates/mlir-asm-proc/src/lib.rs
+++ crates/mlir-asm-proc/src/lib.rs
@@ -1,10 +1,21 @@
 #![deny(warnings)]
 
+use crate::input::MacroInput;
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, spanned::Spanned};
 
 mod input;
 
 #[proc_macro]
-pub fn mlir_asm(_input: TokenStream) -> TokenStream {
-    todo!()
+pub fn mlir_asm(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as MacroInput);
+
+    let context = input.context;
+    let mlir_code = input.mlir_code.span().source_text().unwrap();
+    quote! {
+        melior_next::ir::Module::parse(#context, #mlir_code)
+            .expect("Invalid MLIR code in mlir_asm!() proc macro.")
+    }
+    .into()
 }
