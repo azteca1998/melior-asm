169d4c20f905f74906cee971080b3accd32310e3
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2023-04-15 01:55:19 +0200

    Add compile-time verification.

--- crates/mlir-asm-proc/src/lib.rs
+++ crates/mlir-asm-proc/src/lib.rs
@@ -1,16 +1,23 @@
+#![feature(proc_macro_diagnostic, proc_macro_span_shrink)]
 #![deny(warnings)]
 
-use crate::input::MacroInput;
+use self::{input::MacroInput, verify::verify_mlir};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, spanned::Spanned};
 
 mod input;
+mod verify;
 
 #[proc_macro]
 pub fn mlir_asm(input: TokenStream) -> TokenStream {
+    // Parse macro input.
     let input = parse_macro_input!(input as MacroInput);
 
+    // Verify MLIR code.
+    verify_mlir(input.mlir_code.clone());
+
+    // Generate output.
     let context = input.context;
     let mlir_code = input.mlir_code.span().source_text().unwrap();
     quote! {
