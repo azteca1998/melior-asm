b66ef8b0c44fe7bb99279b2b3811b394ca01e779
Esteve Soler Arderiu <esteve.soler@fratilia.com>
2023-04-17 15:49:13 +0200

    Forward the procedural macro through a normal macro for improved macro hygiene.

--- crates/melior-asm/src/lib.rs
+++ crates/melior-asm/src/lib.rs
@@ -1,1 +1,14 @@
-pub use melior_asm_proc::mlir_asm;
+#[doc(hidden)]
+pub use melior_asm_proc::mlir_asm as mlir_asm_impl;
+#[doc(hidden)]
+pub use melior_next;
+
+#[macro_export]
+macro_rules! mlir_asm {
+    ( $( $inner:tt )* ) => {{
+        use $crate::{melior_next as melior_next, mlir_asm_impl};
+
+        // Forward to the procedural macro.
+        mlir_asm_impl!( $( $inner )* )
+    }};
+}
